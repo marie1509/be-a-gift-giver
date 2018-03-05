17326f7a1aec59fd9c09edeb24aea5e890ebb7b2
marie1509 <32474905+marie1509@users.noreply.github.com>
2018-03-05 14:18:34 +0900

    remove comments of willbuys_controller

--- app/controllers/willbuys_controller.rb
+++ app/controllers/willbuys_controller.rb
@@ -3,21 +3,15 @@ class WillbuysController < ApplicationController
     id = params[:item_id]
     @item = Item.find(id)
     @willbuy = Willbuy.create(item_id: params[:item_id], user_id: current_user.id)
-    # redirect_to items_path
-    # 合計willbuysを表示するため
-    # @willbuys = Willbuy.where(item_id: params[:item_id])
+   
     @willbuy_count = @item.willbuy_count + 1
   end
 
   def destroy
-    # find_byも条件を複数指定できる！
     @item = Item.find(params[:item_id])
     willbuy = Willbuy.find_by(item_id: params[:item_id], user_id: current_user.id)
     willbuy.destroy
-    # redirect_to items_path
-
-    # 合計willbuysを表示するため
-    # @willbuys = Willbuy.where(item_id: params[:item_id])
+  
     @willbuy_count = @item.willbuy_count - 1
   end
 end
